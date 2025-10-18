5745eb1bc5b271c599b8da28a895de77a38dbf2e
Lan071004 <lanie.071004@gmail.com>
2025-10-19 00:41:45 +0700

    init personal information component for profile screen

--- AI_App_FE/SoulMateApp/profile/ProfileScreen.js
+++ AI_App_FE/SoulMateApp/profile/ProfileScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
 import ProfileHeader from './components/ProfileHeader';
-// import PersonalInfo from './components/PersonalInfo';
-// import ZodiacInfo from './components/ZodiacInfo';
+import PersonalInfo from './components/PersonalInfo';
+import ZodiacInfo from './components/ZodiacInfo';
 // import FunctionRow from './components/FunctionRow';
 // import MatchedHistory from './components/MatchedHistory';
 
@@ -10,7 +10,8 @@ export default function ProfileScreen() {
   return (
     <ScrollView style={{ marginTop: 30, backgroundColor: '#000' }}>
       <ProfileHeader />
-      {/* <PersonalInfo />
+      <PersonalInfo />
+      {/*
       <ZodiacInfo />
       <FunctionRow />
       <MatchedHistory /> */}
